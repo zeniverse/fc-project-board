c7c64d5809f3eeffdee27e2208647a150b2cab18
Unknown <0219alice@gmail.com>
2022-07-13 18:02:34 +0900

    #16 - 게시판 view 기본 Frame 작성
    테스트를 통과할 수 있도록 Controller에 핸들러를 작성함.
    index 파일에 기본적인 화면 구성 Frame을 만들어줬음.
    테스트의 Media Type 검사에서 부가적인 옵션이 들어갈 수 있도록 변경해줌.

--- src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -23,7 +23,6 @@ class ArticleControllerTest {
         this.mockMvc = mockMvc;
     }
 
-    @Disabled("구현중")
     @DisplayName("[view][GET] 게시글 리스트 - 게시판 페이지 정상 호출")
     @Test
     public void articlesView() throws Exception {
@@ -32,7 +31,7 @@ class ArticleControllerTest {
         //When + Then
         mockMvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
@@ -46,7 +45,7 @@ class ArticleControllerTest {
         //When + Then
         mockMvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -62,7 +61,7 @@ class ArticleControllerTest {
         mockMvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("articles/search"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 
     @Disabled("구현중")
@@ -75,7 +74,7 @@ class ArticleControllerTest {
         mockMvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("articles/search-hashtag"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 
 }
