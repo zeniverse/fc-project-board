70df5504f6790bd24ccab0a36d61c17449ba555e
Unknown <0219alice@gmail.com>
2022-07-16 18:05:18 +0900

    #21 - update MainController
    오타를 발견해서 오타를 수정해줌

--- src/main/java/com/fastcampus/projectboard/controller/MainController.java
+++ src/main/java/com/fastcampus/projectboard/controller/MainController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     @GetMapping("/")
-    public String root(){
-        return "forword:/articles";
+    public String root() {
+        return "forward:/articles";
     }
 
 }
